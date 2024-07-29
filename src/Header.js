2d08d27236e2c74ff0499cd4aca1c16260ad3a6d
bradydeschamps <xbrady0311@gmail.com>
2024-07-29 14:54:30 -0700

    button add

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,9 @@ function Header() {
   return (
     <header className="header">
       <h1>Brady Deschamps</h1>
+      <a href={`${process.env.PUBLIC_URL}/resume.pdf`} target="_blank" rel="noopener noreferrer">
+        <button className="header-button">View Resume</button>
+      </a>
     </header>
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Profile.css';
 
+//TODOS: Crop main image, add interactivity
 function Profile() {
   return (
     <div className="profile">
       <div className="profile-details">
         <h2>About Me</h2>
-        <p>Hello! I'm Brady Deschamps, a student at the ASU Barett Honors College with a passion for computer science and mathematics. <br></br> I have experience in various technologies including:
+        <p>Hello! I'm Brady Deschamps, a student at the ASU Barett Honors College with a passion for computer science and mathematics. <br></br> I have experience in various languages including:
          <br></br><ul><li>Java</li><li>Python</li><li>Javascript</li><li>React</li><li>Django</li>
             </ul> <br></br>
-            Additionally, I am familiar with multiple platforms including:<ul><li>VSCode</li><li>Pycharm</li><li>Android Studio</li></ul> <br></br>I am currently an intern at 7Stalks Consulting LLC under Steven Klass.</p>
+            Additionally, I am familiar with multiple platforms including:<ul><li>VSCode</li><li>Pycharm</li><li>Android Studio</li></ul> <br></br>I am currently an intern at 7Stalks Consulting LLC under Steven Klass where I mainly work with Python and Django.</p>
         <h2>Contact</h2>
         <ul>
             <li>Email: brady.d.deschamps@gmail.com</li>
